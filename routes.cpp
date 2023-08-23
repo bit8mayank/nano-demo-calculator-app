6fab3e1f26fcadbd7ca74766033371ff7f7e3f4e
bit8mayank <mayankagrawal008@gmail.com>
2023-08-23 23:26:42 +0530

    corrected code

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,19 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello World!"};
 }
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    crow::json::wvalue ans;
+    ans=input[first].i()+input[second].i();
+    return ans;
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    crow::json::wvalue ans;
+    ans=input[first].i()-input[second].i();
+    return ans;
 }
